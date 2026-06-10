07849c0df3610fe24e2cae3f476d79878df3a8c0
Fabiano <fabipiroli@live.com>
2026-06-09 22:16:00 -0300

    migration

--- ControleEstoque/Migrations/20260610011419_AddEnderecoToFornecedor.Designer.cs
+++ ControleEstoque/Migrations/20260610011419_AddEnderecoToFornecedor.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ControleEstoque.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ControleEstoque.Migrations
 {
     [DbContext(typeof(EstoqueContext))]
-    partial class EstoqueContextModelSnapshot : ModelSnapshot
+    [Migration("20260610011419_AddEnderecoToFornecedor")]
+    partial class AddEnderecoToFornecedor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ControleEstoque/Migrations/20260610011419_AddEnderecoToFornecedor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ControleEstoque.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEnderecoToFornecedor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Endereco",
+                table: "Fornecedores",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Endereco",
+                table: "Fornecedores");
+        }
+    }
+}
